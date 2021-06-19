b4884c8e9fb350740e61d9774b8e61ba41642207
niftyy <tusharsparte@gmail.com>
2021-06-19 17:08:31 +0530

    converted validation to hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,22 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email, password
+    }
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email, password
-      });
-
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    doRequest();
   }
 
   return (
@@ -39,14 +38,7 @@ export default () => {
           type="password"
           className="form-control"
         />
-        {errors.length > 0 && (<div className="alert alert-danger">
-          <ul className="my-0">
-            {errors.map(err => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-        )}
+        {errors}
         <button className="btn btn-primary">Sign Up</button>
       </div>
     </form>
