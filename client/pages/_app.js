7c3fae394ea15ac01c1c56eec06777d7bc02d672
niftyy <tusharsparte@gmail.com>
2021-06-19 19:53:16 +0530

    added signin page and signed in status

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
 // next js wraps our components with _apps.js component
 // so we apply css library here
 
-const app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return <div> 
+    <h1>{currentUser.email}</h1>
+    <Component {...pageProps} />
+  </div>;
 };
 
-export default app;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    currentUser: data.currentUser,
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,12 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return <h1>Landing Page!</h1>;
+  return currentUser ? <h1>You are signed in</h1>: <h1>You are not signed in</h1>;
 };
 
 // for fetching data while server side rendering
 LandingPage.getInitialProps = async (context) => {
+  console.log('Landing Page');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser'); 
   
