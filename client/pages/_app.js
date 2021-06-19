57e50a2dd834d7d1657795830200394fb9f07ab7
niftyy <tusharsparte@gmail.com>
2021-06-19 16:36:02 +0530

    added signup frontend

---
+++ client/pages/_app.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+// next js wraps our components with _apps.js component
+// so we apply css library here
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+};
