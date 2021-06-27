81f3fe52a023f846f64d83a45921b616600d1089
niftyy <tusharsparte@gmail.com>
2021-06-27 22:58:08 +0530

    few improvements

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,10 @@
-import buildClient from "../api/build-client";
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are signed in</h1>: <h1>You are not signed in</h1>;
 };
 
 // for fetching data while server side rendering
-LandingPage.getInitialProps = async (context) => {
-  console.log('Landing Page');
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser'); 
-  
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
