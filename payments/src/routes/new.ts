adbeae207751e12ff4eeaa82c2003f009a6a2502
niftyy <tusharsparte@gmail.com>
2021-06-27 16:24:41 +0530

    tying together charge and order in a new model

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@tickster/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -42,12 +43,19 @@ router.post(
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'inr',
       amount: order.price * 100,
       source: token,
       description: "api testing"
     });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id
+    });
+    
+    await payment.save();
 
     res.status(201).send({ success: true });
 });
